449b9b3f1d273788801dd7903dbdfb07ff4a83cd
ToastToastToast <55760757+ToastToastToast@users.noreply.github.com>
2019-10-13 16:53:01 -0700

    dd

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
+function mousePressed(){
+ //variables
+  var x = 50;
+  var y = 25;
+}
+
 function setup() {
 createCanvas(600,600);
+
 }
 
 function draw() {
-background(204);
+background(250, 250, 200);
+  //color RGB
+   fill(3,252,202);
+  stroke(3,136,252);
   //change border thickness
-  strokeWeight(8);
+  strokeWeight(2);
+
+  //drawing function
+  //keyIsPressed also exists
+  if(mouseIsPressed){
+    fill(233,33,33,50);
+    ellipse(mouseX,mouseY,20,20);
+  }
+  else{
+    fill(300,50,50,25);
+    rect(mouseX,mouseY,10,10);
+  }
+
 ellipse(50,50,80,50);
-  point(70,25);
+  point(500,25);
   line(100,120,400,250);
   //y1,y2 point. y3width, y4height
   rect(200,50,100,70);
 
-  strokeWeight(2)
-  //quad x1,y1,x2 y2,x3 y3, x4 y4
-  //Its a flag =D
-  quad(300,310,400,410,250,260,400,260);
-  //Triangle6 x1 y1, x2 y2, x3 y3
-  triangle(350,380,350,454,470,467);
   //arcs x,y,width,height,start,stop
   arc(90,60,80,80,0,HALF_PI);
   arc(390,60,80,80,QUARTER_PI,PI+QUARTER_PI);
+
+  //quad x1,y1,x2 y2,x3 y3, x4 y4
+  //Its a flag =D
+  fill(3,252,165);
+  quad(300,310,400,410,250,260,400,260);
+
+  //Triangle6 x1 y1, x2 y2, x3 y3
+  triangle(300,380,350,454,470,467);
 
 
 }
