87feb0ffcbd3088a4b0591cf3ccd53b9f42c0628
ToastToastToast <55760757+ToastToastToast@users.noreply.github.com>
2019-10-13 17:04:43 -0700

    p

--- sketch.js
+++ sketch.js
@@ -1,12 +1,3 @@
-<html>
-  <head>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js"></script>
-  <script src="sketch.js"></script>
-  </head>
-  <body>
-  </body>
-  </html>
-
 
 function mousePressed(){
  //variables
